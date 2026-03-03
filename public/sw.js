e87e1e6d4df077e5d017f5ea91e3481440127da1
Tron <tron@clawd.bot>
2026-03-03 20:31:03 +0100

    FIX: Remove navigate() from SW kill switch (caused infinite reload loop = black screen)

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,9 @@
-// KILL SWITCH: This service worker unregisters itself and clears all caches
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          console.log('[SW Kill] Deleting cache:', cacheName);
-          return caches.delete(cacheName);
-        })
-      );
-    }).then(() => {
-      console.log('[SW Kill] All caches deleted, unregistering...');
-      return self.registration.unregister();
-    }).then(() => {
-      return self.clients.matchAll();
-    }).then((clients) => {
-      clients.forEach((client) => client.navigate(client.url));
-    })
-  );
-});
+// Kill old service worker: clear caches and unregister
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then(n => Promise.all(n.map(c => caches.delete(c))))
+      .then(() => self.registration.unregister())
+  )
+})
